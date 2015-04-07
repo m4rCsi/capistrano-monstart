94caff94f440b7bda1555548547a5bd4a388c115
Marc Siegenthaler <shin.tower@marcsi.ch>
2015-04-07 19:36:09 +0100

    some more debugging

--- lib/capistrano/monstart.rb
+++ lib/capistrano/monstart.rb
@@ -10,5 +10,5 @@ require 'capistrano/monstart/foreman'
 
 
 load File.expand_path('../tasks/test.cap', __FILE__)
-load File.expand_path('../tasks/monstart.cap', __FILE__)
-load File.expand_path('../tasks/monit.cap', __FILE__)
+#load File.expand_path('../tasks/monstart.cap', __FILE__)
+#load File.expand_path('../tasks/monit.cap', __FILE__)
